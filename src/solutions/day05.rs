34ca8d3d6e7d7b6f18647fc76b9eb3ef1c159a85
Alden Moreton <aldenmoreton99@gmail.com>
2024-12-05 12:56:10 -0600

    Day 5 test cases

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -65,3 +65,222 @@ pub async fn p1(headers: HeaderMap, body: String) -> impl IntoResponse {
     .collect::<String>()
     .into_response()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{router, test_utils::collect_body};
+    use axum::http::{Request, StatusCode};
+    use tower::ServiceExt as _;
+
+    #[tokio::test]
+    async fn part1_1() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "application/toml")
+                    .body(
+                        r#"
+[package]
+name = "not-a-gift-order"
+authors = ["Not Santa"]
+keywords = ["Christmas 2024"]
+
+[[package.metadata.orders]]
+item = "Toy car"
+quantity = 2
+
+[[package.metadata.orders]]
+item = "Lego brick"
+quantity = 230
+"#
+                        .to_string(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = collect_body(response).await;
+
+        assert_eq!(
+            body,
+            "Toy car: 2
+Lego brick: 230"
+        );
+    }
+
+    #[tokio::test]
+    async fn part1_2() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "application/toml")
+                    .body(
+                        r#"
+[package]
+name = "coal-in-a-bowl"
+authors = ["H4CK3R_13E7"]
+keywords = ["Christmas 2024"]
+
+[[package.metadata.orders]]
+item = "Coal"
+quantity = "Hahaha get rekt"
+"#
+                        .to_string(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+    }
+
+    #[tokio::test]
+    async fn part2_1() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "application/toml")
+                    .body(
+                        r#"
+[package]
+name = false
+authors = ["Not Santa"]
+keywords = ["Christmas 2024"]
+"#
+                        .to_string(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
+        let body = collect_body(response).await;
+
+        assert_eq!(body, "Invalid manifest");
+    }
+
+    #[tokio::test]
+    async fn part2_2() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "application/toml")
+                    .body(
+                        r#"
+[package]
+name = "not-a-gift-order"
+authors = ["Not Santa"]
+keywords = ["Christmas 2024"]
+
+[profile.release]
+incremental = "stonks"
+"#
+                        .to_string(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
+        let body = collect_body(response).await;
+
+        assert_eq!(body, "Invalid manifest");
+    }
+
+    #[tokio::test]
+    async fn part3_1() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "application/toml")
+                    .body(
+                        r#"
+[package]
+name = "grass"
+authors = ["A vegan cow"]
+keywords = ["Moooooo"]
+"#
+                        .to_string(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
+        let body = collect_body(response).await;
+
+        assert_eq!(body, "Magic keyword not provided");
+    }
+
+    #[tokio::test]
+    async fn part4_1() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "text/html")
+                    .body(r#"<h1>Hello, bird!</h1>"#.to_string())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    #[tokio::test]
+    async fn part4_2() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/5/manifest")
+                    .method("POST")
+                    .header("Content-Type", "application/yaml")
+                    .body(
+                        r#"
+package:
+  name: big-chungus-sleigh
+  version: "2.0.24"
+  metadata:
+    orders:
+      - item: "Toy train"
+        quantity: 5
+  rust-version: "1.69"
+  keywords:
+    - "Christmas 2024"
+"#
+                        .to_string(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = collect_body(response).await;
+
+        assert_eq!(body, "Toy train: 5");
+    }
+}
